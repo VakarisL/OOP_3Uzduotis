fa6397345dce73d3531fd3e5c8e017f6d28d22e9
Vakaris Likas <vakaris.likas@mif.vu.lt>
2018-02-21 22:41:59 +0200

    innitial vector solution commit

---
+++ Main/Main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cstring>
+#include <algorithm>
+#include <iterator>
+#include <random>
+#include <chrono>
+
+
+//declarations
+//#######################################################################
+
+
+//main
+//#######################################################################
+int main(){
+  
+}
+
+//functions
+//#######################################################################
+
